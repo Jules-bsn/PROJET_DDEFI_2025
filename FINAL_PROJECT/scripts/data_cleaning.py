5589db0fa9b9d46752b0e411d94bcadf290cc917
thomasarecca <151459100+thomasarecca@users.noreply.github.com>
2025-03-06 15:31:06 +0100

    Update data_cleaning.py

--- FINAL_PROJECT/scripts/data_cleaning.py
+++ FINAL_PROJECT/scripts/data_cleaning.py
@@ -2,10 +2,8 @@ import os
 import pandas as pd
 from pipeline import process_pipeline
 
-# Définition des chemins d'accès
 RAW_DATA_PATH = "data/raw/customer_churn_telecom_services.csv"
 PROCESSED_DATA_DIR = "data/processed"
 PROCESSED_DATA_PATH = os.path.join(PROCESSED_DATA_DIR, "cleaned_data.csv")
 
-# Exécution du pipeline de nettoyage et transformation des données
 process_pipeline(RAW_DATA_PATH, PROCESSED_DATA_PATH)
