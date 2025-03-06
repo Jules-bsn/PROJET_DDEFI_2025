5b206732de745943537b4d7911fb06655edb06ee
thomasarecca <151459100+thomasarecca@users.noreply.github.com>
2025-03-06 14:30:31 +0100

    Update train_model_xgboost.py

--- FINAL_PROJECT/scripts/train_model_xgboost.py
+++ FINAL_PROJECT/scripts/train_model_xgboost.py
@@ -5,15 +5,12 @@ from xgboost import XGBClassifier
 import joblib
 import os
 
-# 📚 Chargement des données pré-traitées
 df = pd.read_csv('data/processed/cleaned_data.csv')
 y = df['Churn']
 X = df.drop(columns=['Churn'])
 
-# 📌 Séparation des données
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-# 🎯 Optimisation des hyperparamètres XGBoost
 param_grid_xgb = {
     'learning_rate': [0.01, 0.05, 0.1, 0.2],
     'max_depth': [3, 5, 7, 10]
@@ -24,11 +21,11 @@ xgb_search.fit(X_train, y_train)
 
 print("Best parameters for XGBoost: ", xgb_search.best_params_)
 
-# 📁 Sauvegarde du modèle optimisé
+# Sauvegarde du modèle optimisé
 os.makedirs('deployment', exist_ok=True)
 joblib.dump(xgb_search.best_estimator_, 'deployment/final_model.pkl')
 
-# 📌 Évaluation avec validation croisée
+# validation croisée
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 scores_xgb = RandomizedSearchCV(xgb_search.best_estimator_, param_distributions={}, n_iter=1, cv=cv, scoring='roc_auc')
 scores_xgb.fit(X, y)
